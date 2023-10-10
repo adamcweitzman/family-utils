58b48a2666b4f10075bae49c67080114d770339c
Adam Weitzman <adamcweitzman@gmail.com>
2023-10-10 13:56:14 -0600

    clean up add new board game

--- src/components/AddNewBoardGame.vue
+++ src/components/AddNewBoardGame.vue
@@ -64,7 +64,6 @@ export default {
     const name = ref(null)
     const age = ref(null)
     const accept = ref(true)
-    const isLogging = ref(false)
     const winnerModel = ref<Select>()
     const gameModel = ref<Select>()
     const addGameModel = ref<string>()
@@ -72,85 +71,14 @@ export default {
     const gameOptions: Select[] = []
     const tab = ref('home')
     const gameRows = ref<any[]>([])
-    let tableDataLoaded = false
 
     onBeforeMount(() => {
-      readPlayers()
       readGames()
-      readPlays()
-      // calculateWinningPercentage()
     })
-
-    const logGame = () => {
-      isLogging.value = true
-    }
-
-    const updatePlayerPriority = (player: Player) => {
-      const userRef = db.collection('users').doc(player.id)
-      userRef.set({
-        Name: player.name,
-        Priority: player.priority
-      }, { merge: true })
-    }
-
-    const addGameWon = (id) => {
-      for (const player of players.value) {
-        if (player.id === id) {
-          player.gamesWon++
-        }
-      }
-    }
 
     const isFormIncomplete = computed(() => {
       return !chooserModel.value || !gameModel.value || !winnerModel.value
     })
-
-    const readPlayers = () => {
-      db.collection('users')
-        .get()
-        .then((querySnapshot) => {
-          players.value = []
-          querySnapshot.forEach((user) => {
-            players.value.push({
-              id: user.id,
-              name: user.data().Name,
-              priority: user.data().Priority,
-              gamesPlayed: 0,
-              gamesWon: 0
-            })
-            if (!(user.id in playerIdToName)) {
-              playerIdToName[user.id] = user.data().Name
-            }
-          })
-          players.value.sort((a, b) => a.priority - b.priority)
-        })
-    }
-
-    // const calculateWinningPercentage = () => {
-
-    // }
-
-    const addPlays = () => {
-      players.value.forEach(player => {
-        player.gamesPlayed++
-      })
-    }
-
-    const SortGamesByNumberOfPlays = (plays) => {
-      // 1. Create frequency map
-      const gameFrequencyMap: { [key: string]: number } = {}
-
-      for (const play of plays) {
-        if (gameFrequencyMap[play.gameId]) {
-          gameFrequencyMap[play.gameId]++
-        } else {
-          gameFrequencyMap[play.gameId] = 1
-        }
-      }
-
-      // 2. Sort keys by their frequencies
-      sortedGames.value = Object.keys(gameFrequencyMap).sort((a, b) => gameFrequencyMap[b] - gameFrequencyMap[a])
-    }
 
     const resetForm = () => {
       gameModel.value = undefined
@@ -183,116 +111,12 @@ export default {
         })
     }
 
-    const readPlays = () => {
-      db.collection('plays')
-        .get()
-        .then((querySnapshot) => {
-          querySnapshot.forEach((play) => {
-            plays.push({
-              chooserId: play.data().chooserId,
-              winnerId: play.data().winnerId,
-              gameId: play.data().gameId
-            })
-          })
-          const gameIdToCount: { [key: string] : number } = {}
-          const gameIdToArrayIndex: { [key: string] : number } = {}
-          const playerWins: any[][] = []
-          SortGamesByNumberOfPlays(plays)
-          console.log(sortedGames)
-          plays.forEach((play) => {
-            addPlays()
-            if (play.chooserId === players.value[0].id) {
-              if (!gameIdToCount[play.gameId]) {
-                gameIdToCount[play.gameId] = 1
-              } else {
-                gameIdToCount[play.gameId]++
-              }
-            }
-            if (!(play.gameId in gameIdToArrayIndex)) {
-              gameIdToArrayIndex[play.gameId] = playerWins.length
-              switch (play.winnerId) {
-                case 'UyfZTqM1ZYqkAS31UPQ9':
-                  playerWins.push([gameIdToName[play.gameId], 1, 0, 0, 0])
-                  addGameWon('UyfZTqM1ZYqkAS31UPQ9')
-                  break
-                case "DsnaBf8FyLfsRbNw1txQ":
-                  playerWins.push([gameIdToName[play.gameId], 0, 1, 0, 0])
-                  addGameWon("DsnaBf8FyLfsRbNw1txQ")
-                  break
-                case "t9rCulN2SuSP7ynC0UQx":
-                  playerWins.push([gameIdToName[play.gameId], 0, 0, 1, 0])
-                  addGameWon("t9rCulN2SuSP7ynC0UQx")
-                  break
-                case "GnQ3MhXqB9WSr8LB5hm9":
-                  playerWins.push([gameIdToName[play.gameId], 0, 0, 0, 1])
-                  addGameWon("GnQ3MhXqB9WSr8LB5hm9")
-                  break
-                default:
-                  break
-              }
-            } else {
-              const gameIndex = gameIdToArrayIndex[play.gameId]
-              switch (play.winnerId) {
-                case 'UyfZTqM1ZYqkAS31UPQ9':
-                  playerWins[gameIndex][1]++
-                  addGameWon('UyfZTqM1ZYqkAS31UPQ9')
-                  break
-                case "DsnaBf8FyLfsRbNw1txQ":
-                  playerWins[gameIndex][2]++
-                  addGameWon("DsnaBf8FyLfsRbNw1txQ")
-                  break
-                case "t9rCulN2SuSP7ynC0UQx":
-                  playerWins[gameIndex][3]++
-                  addGameWon("t9rCulN2SuSP7ynC0UQx")
-                  break
-                case "GnQ3MhXqB9WSr8LB5hm9":
-                  playerWins[gameIndex][4]++
-                  addGameWon("GnQ3MhXqB9WSr8LB5hm9")
-                  break
-                default:
-                  break
-              }
-            }
-            // populate the data table
-            // save the index of the game array in a dictionary if it doesn't exist in the dictionary
-            // save player id to spot in array with a dictionary { ashley 1, noah 2, debbie 3, adam 4}
-            // first lookup index of session if exists in dict
-            // lookup the winner id in the dict and ++ the spot in the array
-            // else
-            // add to the dictionary the array.length - 1
-            // add new row to the dictionary
-          })
-          playerWins.forEach(game => {
-            gameRows.value.push({ name: game[0], ashley: game[1], debbie: game[2], noah: game[3], adam: game[4] })
-          })
-          tableDataLoaded = true
-          let highestValue = Number.MIN_SAFE_INTEGER
-          let highestValueRecord = { key: '', value: 0 }
-          for (const [key, value] of Object.entries(gameIdToCount)) {
-            if (value > highestValue) {
-              highestValue = value
-              highestValueRecord = { key, value }
-            }
-          }
-          games.value.forEach(game => {
-            if (game.id === highestValueRecord.key) {
-              favoriteGame.value = game.name
-            }
-          })
-        })
-
-      loaded = true
-    }
-
     return {
       name,
       age,
       accept,
       players,
-      readPlayers,
       readGames,
-      isLogging,
-      logGame,
       gameOptions,
       isFormIncomplete,
       gameModel,
@@ -305,24 +129,6 @@ export default {
       gameIdToName,
       loaded,
       gameRows,
-      gameRow: [
-        { name: "Game 1", ashley: 0, noah: "0", adam: "1", debbie: "0" },
-        { name: "Game 2", ashley: "0", noah: "0", adam: "1", debbie: "0" }
-        // ... more game objects
-      ],
-      tableColumns: [
-        { name: "Ashley", required: true, label: "game", align: "left", field: "name", sortable: true },
-        { name: "Ashley", required: true, label: "ashley", align: "left", field: "ashley", sortable: true },
-        { name: "Noah", required: true, label: "noah", align: "left", field: "noah", sortable: true },
-        { name: "Adam", required: true, label: "adam", align: "left", field: "adam", sortable: true },
-        { name: "Debbie", required: true, label: "debbie", align: "left", field: "debbie", sortable: true }
-      ],
-      playerOptions: [
-        { value: 'GnQ3MhXqB9WSr8LB5hm9', label: 'Adam' },
-        { value: 'DsnaBf8FyLfsRbNw1txQ', label: 'Debbie' },
-        { value: 't9rCulN2SuSP7ynC0UQx', label: 'Noah' },
-        { value: 'UyfZTqM1ZYqkAS31UPQ9', label: 'Ashley' }
-      ],
 
       onSubmitGame () {
         console.log(addGameModel.value)
@@ -344,65 +150,6 @@ export default {
               icon: 'cloud_done'
             })
           })
-      },
-      onSubmit () {
-        if (accept.value !== true) {
-          $q.notify({
-            color: 'red-5',
-            textColor: 'white',
-            icon: 'warning',
-            message: 'You need to accept the license and terms first'
-          })
-        }
-        try {
-          const chooserId = chooserModel.value?.value
-          const chooserPriority = players.value.find(x => x.id === chooserId)?.priority
-
-          if (!chooserPriority) {
-            throw new Error('chooser priority is undefined')
-          }
-
-          const playerTurnId = players.value[0].id
-
-          db.collection('plays').add({
-            chooserId,
-            winnerId: winnerModel.value?.value,
-            gameId: gameModel.value?.value
-          })
-          if (chooserId !== playerTurnId) {
-            players.value.forEach(player => {
-              if (player.id === playerTurnId) {
-                return
-              } else if (player.id === chooserId) {
-                player.priority = players.value.length
-              } else if (player.priority > chooserPriority) {
-                player.priority--
-              }
-              updatePlayerPriority(player)
-            })
-          } else {
-            players.value.forEach(player => {
-              if (player.priority === 1) {
-                player.priority = players.value.length
-              } else {
-                player.priority--
-              }
-              updatePlayerPriority(player)
-            })
-          }
-          $q.notify({
-            color: 'positive',
-            message: 'You logged a play successfully!',
-            icon: 'cloud_done'
-          })
-          readPlayers()
-          resetForm()
-        } catch (error) {
-          $q.notify({
-            color: 'negative',
-            message: `Error adding document: ${error}`
-          })
-        }
       },
       onReset () {
         resetForm()
